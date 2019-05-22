8f7023e91f5e5789f3018b6194db725862951fe5
Justin <sumbhodi1618@gmail.com>
2019-05-22 07:56:01 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,13 +1,13 @@
 def bonus
   epic_tragedy = {
-   :montague => {:patriarch {},
-:matriarch{},
-:hero{},
-:hero_friends{} },
-   :capulet => { :patriarch {},
-:matriarch{},
-:hero{},
-:hero_friends{}}
+   :montague => {:patriarch {},=>
+:matriarch{},=>
+:hero{},=>
+:hero_friends =>[]{} },
+   :capulet => { :patriarch{},=>
+:matriarch{},=>
+:heroine{},=>
+:heroine_friends =>[]{}
 }
   #code your solution here:
   
