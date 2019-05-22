eb32519b8be3051fb3010f14bc5fc13c9207c18a
Justin <sumbhodi1618@gmail.com>
2019-05-22 08:07:29 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -5,11 +5,15 @@ def bonus
       :hero{},=>
         :hero_friends =>[]
   },
-:capulet => { :patriarch{},=>
- :matriarch{},=>
-   :heroine{},=>
-     :heroine_friends =>[]
-  }
+:capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
 }
   #code your solution here:
   
